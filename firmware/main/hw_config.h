699cb5a62590b671e088f47db3213ebf928764f3
george benett <georgebenett@gmail.com>
2026-02-23 18:53:42 +0100

    Run clang-format on hw_config module

--- firmware/main/hw_config.h
+++ firmware/main/hw_config.h
@@ -4,31 +4,31 @@
 #include "driver/gpio.h"
 
 // ADC channel definitions
-#define THROTTLE_PIN                ADC_CHANNEL_8
-#define BRAKE_PIN                   ADC_CHANNEL_7
+#define THROTTLE_PIN ADC_CHANNEL_8
+#define BRAKE_PIN ADC_CHANNEL_7
 
 // Battery ADC channel
-#define BATTERY_VOLTAGE_PIN         ADC_CHANNEL_0
+#define BATTERY_VOLTAGE_PIN ADC_CHANNEL_0
 
 // Button GPIO definitions
-#define MAIN_BUTTON_GPIO            GPIO_NUM_10
+#define MAIN_BUTTON_GPIO GPIO_NUM_10
 
 // Power off GPIO definition
-#define POWER_HOLD_GPIO             GPIO_NUM_4
+#define POWER_HOLD_GPIO GPIO_NUM_4
 
 // Battery status GPIO
-#define BATTERY_IS_CHARGING_GPIO    GPIO_NUM_2
-#define BATTERY_PROBE_PIN           GPIO_NUM_7
+#define BATTERY_IS_CHARGING_GPIO GPIO_NUM_2
+#define BATTERY_PROBE_PIN GPIO_NUM_7
 
 // TFT display GPIO definitions
-#define TFT_MOSI_PIN                GPIO_NUM_17
-#define TFT_SCLK_PIN                GPIO_NUM_16
-#define TFT_CS_PIN                  GPIO_NUM_15
-#define TFT_DC_PIN                  GPIO_NUM_14
-#define TFT_RST_PIN                 GPIO_NUM_18
-#define TFT_BL_PIN                  GPIO_NUM_13
+#define TFT_MOSI_PIN GPIO_NUM_17
+#define TFT_SCLK_PIN GPIO_NUM_16
+#define TFT_CS_PIN GPIO_NUM_15
+#define TFT_DC_PIN GPIO_NUM_14
+#define TFT_RST_PIN GPIO_NUM_18
+#define TFT_BL_PIN GPIO_NUM_13
 
 // Viber GPIO definition
-#define VIBER_PIN                   GPIO_NUM_6
+#define VIBER_PIN GPIO_NUM_6
 
 #endif // HW_CONFIG_H
